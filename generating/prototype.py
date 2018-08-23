51dfc995014493283cde2c017727e50816c50591
Denis Belavin <belavin.d.andreevich@gmail.com>
2018-08-23 11:05:32 +0300

    fix copy

--- generating/prototype.py
+++ generating/prototype.py
@@ -10,6 +10,9 @@
 """
 
 
+import copy
+
+
 class Prototype(object):
     def __init__(self):
         self._objects = dict()
@@ -21,7 +24,7 @@ class Prototype(object):
         del self._objects[name]
 
     def clone(self, name, attr):
-        obj = self._objects[name]
+        obj = copy.deepcopy(self._objects[name])
         obj.__dict__.update(attr)
         return obj
 
